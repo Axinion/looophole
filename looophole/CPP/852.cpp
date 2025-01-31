d1938ae0078f0c7cf7345538e34d27dd2dec5c74
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-01-31 10:36:53 -0600

    Update 852.cpp

--- looophole/CPP/852.cpp
+++ looophole/CPP/852.cpp
@@ -1,3 +1,5 @@
+// Solution 1
+
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
@@ -13,6 +15,26 @@ public:
             }
             else if(arr[mid]>arr[mid+1]){
                 e = mid-1;
+            }
+        }
+        return s;
+    }
+};
+
+// Solution 2
+
+lass Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int s = 0;
+        int e = arr.size()-1;
+        while(s<e){
+            int mid = s + ((e-s)/2);
+            if(arr[mid]<arr[mid+1]){
+                s = mid + 1;
+            }
+            else{
+                e = mid;
             }
         }
         return s;
