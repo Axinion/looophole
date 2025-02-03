33d64cc0541c66da2ac8cfef254bd1121d4e3572
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-02-03 11:48:30 -0600

    Update Search_rotated_sorted_array.cpp

--- looophole/CPP/Search_rotated_sorted_array.cpp
+++ looophole/CPP/Search_rotated_sorted_array.cpp
@@ -1,3 +1,5 @@
+// Solution 1
+
 int binarySearch(vector<int>& arr, int s, int e, int key) {
 
     int start = s;
@@ -45,3 +47,36 @@ int search(vector<int>& arr, int n, int k)
         binarySearch(arr, 0, pivot, k);
     }
 }
+
+// Solution 2
+/*
+int search(vector<int>& arr, int n, int k)
+{
+    int s = 0;
+    int e = n-1;
+    while(s<=e){
+        int mid = s + ((e-s)/2);
+        if(arr[mid]==k){
+            return mid;
+        }
+        if(arr[s]<=arr[mid]){
+            if(k>=arr[s] && k<arr[mid]){
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+        }
+        else{
+            if(k>arr[mid] && k<=arr[e]){
+                s = mid+1;
+            }
+            else{
+                e = mid-1;
+            }
+        }
+    }
+    return -1;
+}
+
+*/
