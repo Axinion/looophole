f38b30f971ac308cf1ac1cab87859be604e8255d
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-02-05 17:18:08 -0600

    Update BubbleSort.cpp

--- looophole/CPP/BubbleSort.cpp
+++ looophole/CPP/BubbleSort.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h> 
 void bubbleSort(vector<int>& arr, int n)
 {   
-    int i=1;
-    while(i<n){
-        for(int j=0;j<n-i;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+    for(int i = 0; i < n; i++) {
+        bool swapped = false;
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
         }
-        i+=1;
+        if(!swapped) break;
     }
 }
