14d8d80c998295bb36afb196c1cab0e5e11e45fe
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-02-07 12:19:00 -0600

    Update 75.cpp

--- looophole/CPP/75.cpp
+++ looophole/CPP/75.cpp
@@ -12,3 +12,27 @@ public:
         }
     }
 };
+
+// Solution 2 -> Insertion Sort
+/*
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n= nums.size();
+        for(int i=0;i<n;i++){
+            int temp = nums[i];
+            int j=i-1;
+            while(j>=0){
+                if(nums[j]>temp){
+                    nums[j+1] = nums[j];
+                }
+                else{
+                    break;
+                }
+                j-=1;
+            }
+            nums[j+1] = temp;
+        }
+    }
+};
+*/
