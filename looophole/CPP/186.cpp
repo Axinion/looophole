104c8e5cea286c23dd5d7360aca8d3d112d27c9e
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-02-12 11:50:20 -0600

    Update 186.cpp

--- looophole/CPP/186.cpp
+++ looophole/CPP/186.cpp
@@ -2,26 +2,21 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm> // For reverse
+#include <algorithm>
 using namespace std;
 
 void reverseWords(vector<char>& s) {
     int n = s.size();
-
-    // Step 1: Reverse the entire string
     reverse(s.begin(), s.end());
-
-    // Step 2: Reverse each word in the reversed string
-    int start = 0; // Start index of a word
+    int start = 0;
     for (int end = 0; end < n; end++) {
         if (s[end] == ' ' || end == n - 1) {
-            // If we find a space or reach the end of the string, reverse the word
             if (s[end] == ' ') {
                 reverse(s.begin() + start, s.begin() + end);
             } else {
                 reverse(s.begin() + start, s.begin() + end + 1);
             }
-            start = end + 1; // Move to the next word
+            start = end + 1;
         }
     }
 }
