26971ca78d137c0ae858da4c785b1baa232564ae
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-02-18 16:25:01 -0600

    Update 186.cpp

--- looophole/CPP/186.cpp
+++ looophole/CPP/186.cpp
@@ -21,6 +21,23 @@ void reverseWords(vector<char>& s) {
     }
 }
 
+
+void reverseWords1(vector<char>& s){
+    int n = s.size();
+    int st = 0;
+    for(int end = 0; end<n; end++){
+        if(s[end] == ' ' || end == n-1){
+            if(s[end] == ' '){
+                reverse(s.begin()+st, s.begin() + end);
+            }
+            else{
+                reverse(s.begin()+st, s.begin() + end+1);
+            }
+            st = end + 1;
+        }
+    }
+}
+
 int main() {
     vector<char> s = {'t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e'};
 
@@ -29,10 +46,25 @@ int main() {
     cout << endl;
 
     reverseWords(s);
+    
 
+    cout << "Reversed: ";
+    for (char c : s) cout << c;
+    cout << endl;
+    
+    reverseWords1(s);
+    
     cout << "Reversed: ";
     for (char c : s) cout << c;
     cout << endl;
 
     return 0;
 }
+
+
+// Output
+/*
+Original: the sky is blue
+Reversed: blue is sky the
+Reversed: eulb si yks eht
+*/
