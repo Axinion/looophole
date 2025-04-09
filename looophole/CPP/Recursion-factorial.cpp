7968633b6be9137b9afa234865f94560ecc84d49
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-04-09 15:40:15 -0500

    Create Recursion-factorial.cpp

---
+++ looophole/CPP/Recursion-factorial.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fact(int n){
+    if(n==0){
+        return 1;
+    }
+    return n*fact(n-1);
+}
+
+int main() {
+	int a;
+	cin>>a;
+	
+	cout<<fact(a)<<endl;
+
+}
