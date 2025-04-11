ae47f62783e23dd5dc4a4f8198acbffbcdba8c50
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-04-10 20:15:28 -0500

    Create  Count Ways To Reach The N-th Stairs.cpp

---
+++ looophole/CPP/Count Ways To Reach The N-th Stairs.cpp
@@ -0,0 +1,16 @@
+// Solution 1: (Only recursion)
+
+#include <bits/stdc++.h> 
+int countDistinctWays(int nStairs) {
+    if(nStairs<0){
+        return 0;
+    }
+    if(nStairs==0){
+        return 1;
+    }
+    int ans = countDistinctWays(nStairs-1) + countDistinctWays(nStairs-2);
+    return ans;
+}
+
+// Solution 2: (Recursion & DP)
+
