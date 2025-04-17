0db544c4545b44fa417c7dbf1d0c8422918b4937
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-04-17 01:59:57 -0500

    Update ReverseString(Recursion).cpp

--- looophole/CPP/ReverseString(Recursion).cpp
+++ looophole/CPP/ReverseString(Recursion).cpp
@@ -1,3 +1,5 @@
+// Solution 1 -> 2 Pointers
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -17,5 +19,28 @@ int main() {
     int i = 0;
     int j = s.size()-1;
     reverse(i,j,s);
+    cout<<s;
+}
+
+// Solution 2 -> using 1 pointer
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverse(int i, int size, string& s){
+    if(i>size-i-1){
+        return;
+    }
+    swap(s[i], s[size-i-1]);
+    i++;
+    reverse(i, size, s);
+}
+
+int main() {
+    string s;
+    cin>>s;
+    int i = 0;
+    int sz = s.size();
+    reverse(i,sz,s);
     cout<<s;
 }
