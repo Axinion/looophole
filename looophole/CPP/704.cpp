ef566c2d85376a8165559b10ec8d459966859a6d
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-04-20 02:10:35 -0500

    Update 704.cpp

--- looophole/CPP/704.cpp
+++ looophole/CPP/704.cpp
@@ -1,3 +1,5 @@
+// Solution 1: (without recursion)
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -16,5 +18,37 @@ public:
             }
         }
         return -1;
+    }
+};
+
+
+// Solution 2: (with recursion)
+
+class Solution {
+
+public:
+    int Bs(vector<int>& nums, int s, int e, int t){
+        if(s>e){
+            return -1;
+        }
+        int mid = s + (e-s)/2;
+        if(nums[mid]==t){
+            return mid;
+        }
+        else if(nums[mid]>t){
+            return Bs(nums, s, mid-1, t);
+        }
+        else{
+            return Bs(nums, mid+1, e, t);
+        }
+        return -1;
+    }
+
+public:
+    int search(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size()-1;
+        int res = Bs(nums, s, e, target);
+        return res;
     }
 };
