0553a77efa3317cde34202d0d1a16c80f07ed92d
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-04-22 03:17:49 -0500

    Update 88.cpp

--- looophole/CPP/88.cpp
+++ looophole/CPP/88.cpp
@@ -10,3 +10,29 @@ public:
         nums1 = nums2;
     }
 };
+
+// Solution 2: (better time complexity)
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> res;
+        int i=0;
+        int j=0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                res.push_back(nums1[i++]);
+            }
+            else{
+                res.push_back(nums2[j++]);
+            }
+        }
+        while(i<m){
+            res.push_back(nums1[i++]);
+        }
+        while(j<n){
+            res.push_back(nums2[j++]);
+        }
+        nums1 = res;
+    }
+};
