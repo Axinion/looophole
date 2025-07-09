d5ef5f9af6ec0440ef4e5700506ee0b7c5930230
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-07-09 09:31:43 -0500

    Update 231.cpp

--- looophole/CPP/231.cpp
+++ looophole/CPP/231.cpp
@@ -1,3 +1,5 @@
+// Solution 1 -> Recursion
+
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
@@ -5,5 +7,48 @@ public:
             return false;
         }
         return ((n)&(n-1))==0;
+    }
+};
+
+// Solution 2 -> Recursion
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        // base case
+        if(n==1){
+            return true;
+        }
+        else if(n==0 or n%2!=0){
+            return false;
+        }
+
+        return isPowerOfTwo(n/2);
+    }
+};
+
+// Solution 3 -> Without Recursion
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int  count = 0;
+        int bit;
+        if(n<0){
+            return false;
+        }
+        while(n!=0){
+            bit = n&1;
+            if(bit==1){
+                count += 1;
+            }
+            n = n>>1;
+        }
+        if(count == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 };
