a8b0e3e7b342f1bd9e965b345fd74d3257b00b55
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-07-24 09:55:40 -0500

    Update BinarySearchRecursion.cpp

--- looophole/CPP/BinarySearchRecursion.cpp
+++ looophole/CPP/BinarySearchRecursion.cpp
@@ -71,3 +71,29 @@ class Solution {
 };
 
 
+// Code Ninjas
+
+int bs(vector<int>& nums, int s, int e, int target){
+    // base case
+    if(s>e){
+        return -1;
+    }
+    int mid = s + (e-s)/2;
+    if(nums[mid]==target){
+        return mid;
+    }
+    if(nums[mid]>target){
+        return bs(nums, s, mid-1, target);
+    }
+    else{
+        return bs(nums, mid+1, e, target);
+    }
+}
+
+
+
+int search(vector<int> &nums, int target) {
+    // Write your code here.
+    int ans = bs(nums, 0, nums.size()-1, target);
+    return ans; 
+}
