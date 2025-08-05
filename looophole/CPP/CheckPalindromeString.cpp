77a061abaa46589c0ff4a1d82bb8c4c2e272bfa7
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-08-05 16:49:00 -0500

    Update CheckPalindromeString.cpp

--- looophole/CPP/CheckPalindromeString.cpp
+++ looophole/CPP/CheckPalindromeString.cpp
@@ -1,5 +1,5 @@
 // Recursion
-
+// Solution 1
 string value(string& str){
     if(str.empty()){
         return "";
@@ -14,3 +14,28 @@ bool isPalindrome(string& str) {
     string ans = value(temp);
     return ans == str;
 }
+
+// Solution 2 (GFG)
+
+class Solution {
+  public:
+  
+    bool check(int i, int j, string& s){
+        //base case
+        if(i>j){
+            return true;
+        }
+        if(s[i]!=s[j]){
+            return false;
+        }
+        else{
+            return check(i+1, j-1, s);
+        }
+    }
+  
+    bool isPalindrome(string& s) {
+        // code here
+        bool ans = check(0, s.length()-1, s);
+        return ans;
+    }
+};
