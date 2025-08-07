deff4580d084406e2f5166f224aaee9b828c2dbf
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-08-06 23:29:39 -0500

    Update 75.cpp

--- looophole/CPP/75.cpp
+++ looophole/CPP/75.cpp
@@ -13,12 +13,37 @@ public:
     }
 };
 
+// Bubble sort with Recursion
+
+class Solution {
+public:
+
+    void sort(vector<int>& nums, int n){
+        // base case
+        if(n==0 || n==1){
+            return;
+        }
+        for(int i=0; i<n-1;i++){
+            if(nums[i]>nums[i+1]){
+                swap(nums[i], nums[i+1]);
+            }
+        }
+        sort(nums, n-1);
+    }
+
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums, n);
+    }
+};
+
+
 // Solution 2 -> Insertion Sort
 /*
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n= nums.size();
+        int n = nums.size();
         for(int i=0;i<n;i++){
             int temp = nums[i];
             int j=i-1;
