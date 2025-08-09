9a3a3d924384603c186d744daf708477aaf2fc59
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-08-09 00:12:33 -0500

    Update Selection_Sort.cpp

--- looophole/CPP/Selection_Sort.cpp
+++ looophole/CPP/Selection_Sort.cpp
@@ -1,3 +1,5 @@
+// Solution 1
+
 void selectionSort(vector<int>&arr) {
     for(int i=0;i<arr.size()-1;i++){
         int min = i;
@@ -8,4 +10,28 @@ void selectionSort(vector<int>&arr) {
         }
         swap(arr[min], arr[i]);
     }
+}
+
+// Solution 2 (Recursion)
+
+#include <bits/stdc++.h>
+
+void sort(vector<int>& arr, int n, int start){
+    // base case
+    if(start>=n-1){
+        return;
+    }
+    int min = start;
+    for(int i = start; i<n; i++){
+        if(arr[i]<arr[min]){
+            min = i;
+        }
+    }
+    swap(arr[start], arr[min]);
+    sort(arr, n, start+1);
+}
+
+void selectionSort(vector<int>& arr, int n)
+{   
+    sort(arr, n, 0);
 }
