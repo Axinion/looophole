9ebc1eb91299d01c29bcd2e6ff4c6384826a2b89
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-08-18 04:03:39 -0500

    Update 242.cpp

--- looophole/CPP/242.cpp
+++ looophole/CPP/242.cpp
@@ -1,23 +1,21 @@
-// Solution 1: (Without Hashmap)
+// Solution 1: (Without HashMap)
 
 class Solution {
 public:
     bool isAnagram(string s, string t) {
         int a[26] = {0};
         int b[26] = {0};
-        for(int i=0;i<s.size();i++){
-            a[s[i]- 'a']++;
+        int z = 0;
+        for(char i:s){
+          a[i-'a']++;
         }
-        for(int i=0;i<t.size();i++){
-            b[t[i]- 'a']++;
+        for(char i:t){
+          b[i-'a']++;
         }
-        for(int i=0; i<26;i++){
-            if(a[i]!=b[i]){
-                return false;
-            }
-            else{
-                continue;
-            }
+        for(int i=0;i<26;i++){
+          if(a[i]!=b[i]){
+            return false;
+          }
         }
         return true;
     }
@@ -26,3 +24,46 @@ public:
 
 // Solution 2:
 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+          return false;
+        }
+        int freq[26] = {0};
+        for(int i=0;i<s.length();i++){
+          freq[s[i]-'a']++;
+          freq[t[i]-'a']--;
+        }
+        for(int i:freq){
+          if(i!=0){
+            return false;
+          }
+        }
+        return true;
+    }
+};
+
+// Solution 3: (Sort and compare)
+
+// Solution 4: (Using HashTable)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+          return false;
+        }
+        unordered_map<char, int> freq;
+        for(int i=0;i<s.length();i++){
+          freq[s[i]-'a']++;
+          freq[t[i]-'a']--;
+        }
+        for(auto i:freq){
+          if(i.second!=0){
+            return false;
+          }
+        }
+        return true;
+    }
+};
