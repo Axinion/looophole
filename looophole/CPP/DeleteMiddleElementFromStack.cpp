837e0e0dd05f5ebb76b72f1804f14268bc3a579c
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-09-10 11:57:23 -0500

    Create DeleteMiddleElementFromStack.cpp

---
+++ looophole/CPP/DeleteMiddleElementFromStack.cpp
@@ -0,0 +1,24 @@
+// Solution 1 - recursion
+
+#include <bits/stdc++.h> 
+
+void solve(stack<int>&inputStack, int count, int size){
+   // base case:
+   if(count==size/2){
+      inputStack.pop();
+      return;
+   }
+
+   int num = inputStack.top();
+   inputStack.pop();
+
+   solve(inputStack, count+1, size);
+
+   inputStack.push(num);
+}
+
+void deleteMiddle(stack<int>&inputStack, int N){
+	
+   solve(inputStack, 0, N);
+   
+}
