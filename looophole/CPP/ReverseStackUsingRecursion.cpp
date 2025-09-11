b9d97e73cbd2f6333df08498d484f386719dcf4d
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-09-10 19:54:40 -0500

    Create ReverseStackUsingRecursion.cpp

---
+++ looophole/CPP/ReverseStackUsingRecursion.cpp
@@ -0,0 +1,28 @@
+// Solution 1 -> Reverse stack and add element at bottom
+
+void insertAtBottom(stack<int> &stack, int x){
+    if(stack.empty()){
+        stack.push(x);
+        return;
+    }
+
+    int num = stack.top();
+    stack.pop();
+
+    insertAtBottom(stack, x);
+
+    stack.push(num);
+}
+
+void reverseStack(stack<int> &stack) {
+    if(stack.empty()){
+        return;
+    }
+
+    int num = stack.top();
+    stack.pop();
+
+    reverseStack(stack);
+
+    insertAtBottom(stack, num);
+}
