d842deda4de88cbe603b855e5dc4c313ceee0df4
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-09-12 11:22:50 -0500

    Update 136.cpp

--- looophole/CPP/136.cpp
+++ looophole/CPP/136.cpp
@@ -8,3 +8,19 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.count(num)) {
+                seen.erase(num);
+            } else {
+                seen.insert(num);
+            }
+        }
+        return *seen.begin();
+    }
+};
