451392d1b0b55e9b7ed3830dc9b3fc0d7571636a
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-09-14 02:40:56 -0500

    Create 100.cpp

---
+++ looophole/CPP/100.cpp
@@ -0,0 +1,39 @@
+// Solution 1: Recursion + comparison
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // base case
+        if(p==NULL && q==NULL){
+            return true;
+        }
+        else if(p!=NULL && q==NULL){
+            return false;
+        }
+        else if(p==NULL && q!=NULL){
+            return false;
+        }
+
+        bool left = isSameTree(p->left, q->left);
+        bool right = isSameTree(p->right, q->right);
+
+        bool curr = p->val == q->val;
+
+        if(left && right && curr){
+            return true;
+        }
+        return false;
+    }
+};
