c44e4b3d6fefac8606be1dcb2784d0159266141a
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-09-28 04:40:39 -0500

    Update heapImplementationUsingArray.cpp

--- looophole/CPP/heapImplementationUsingArray.cpp
+++ looophole/CPP/heapImplementationUsingArray.cpp
@@ -20,7 +20,7 @@ class heap{
             
             while(index>1){
                 int parent = index/2;
-                if(arr[parent]<arr[index]){
+                if(arr[index]>arr[parent]){
                     swap(arr[parent], arr[index]);
                     index = parent;
                 }
@@ -40,12 +40,10 @@ class heap{
         void deleteFromHeap(){
             if(size==0){
                 cout<<"Nothing to Delete"<<endl;
-                return;
             }
             arr[1] = arr[size];
             size--;
-            
-            int i=1;
+            int i = 1;
             while(i<size){
                 int left = i*2;
                 int right = i*2 + 1;
@@ -66,26 +64,26 @@ class heap{
 
 void heapify(int arr[], int n, int i){
     int largest = i;
-    int left = 2*i;
-    int right = 2*i + 1;
+    int left = i*2;
+    int right = i*2 + 1;
     
-    if(left<n && arr[largest]<arr[left]){
+    if(left<=n && arr[largest]<arr[left]){
         largest = left;
     }
-    if(right<n && arr[largest]<arr[right]){
+    if(right<=n && arr[largest]<arr[right]){
         largest = right;
     }
     
     if(largest!=i){
-        swap(arr[largest], arr[i]);
+        swap(arr[i], arr[largest]);
         heapify(arr, n, largest);
     }
 }
-
+        
 void minHeapify(int arr[], int n, int i){
     int smallest = i;
-    int left = 2*i;
-    int right = 2*i + 1;
+    int left = i*2;
+    int right = i*2 + 1;
     
     if(left<=n && arr[smallest]>arr[left]){
         smallest = left;
@@ -95,10 +93,20 @@ void minHeapify(int arr[], int n, int i){
     }
     
     if(smallest!=i){
-        swap(arr[smallest], arr[i]);
+        swap(arr[i], arr[smallest]);
         minHeapify(arr, n, smallest);
     }
 }
+
+void heapSort(int arr[], int n){
+    int size = n;
+    while(size>1){
+        swap(arr[size], arr[1]);
+        size--;
+        heapify(arr, size, 1);
+    }
+}
+        
 
 int main() {
 	heap h;
@@ -119,6 +127,12 @@ int main() {
 	for(int i = n/2;i>0;i--){
 	    heapify(arr, n, i);
 	}
+	for(int i=1;i<=n;i++){
+	    cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+	
+	heapSort(arr, n);
 	for(int i=1;i<=n;i++){
 	    cout<<arr[i]<<" ";
 	}
