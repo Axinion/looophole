6cf2c69cdab8ef7c64b02b5d344db33a13813d2d
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-09-28 02:50:30 -0500

    Update heapImplementationUsingArray.cpp

--- looophole/CPP/heapImplementationUsingArray.cpp
+++ looophole/CPP/heapImplementationUsingArray.cpp
@@ -34,9 +34,71 @@ class heap{
             for(int i=1;i<=size;i++){
                 cout<<arr[i]<<" ";
             }
+            cout<<endl;
+        }
+        
+        void deleteFromHeap(){
+            if(size==0){
+                cout<<"Nothing to Delete"<<endl;
+                return;
+            }
+            arr[1] = arr[size];
+            size--;
+            
+            int i=1;
+            while(i<size){
+                int left = i*2;
+                int right = i*2 + 1;
+                if(left<size && arr[i]<arr[left]){
+                    swap(arr[i], arr[left]);
+                    i = left;
+                }
+                else if(right<size && arr[i]<arr[right]){
+                    swap(arr[i], arr[right]);
+                    i = right;
+                }
+                else{
+                    return;
+                }
+            }
         }
 };
 
+void heapify(int arr[], int n, int i){
+    int largest = i;
+    int left = 2*i;
+    int right = 2*i + 1;
+    
+    if(left<n && arr[largest]<arr[left]){
+        largest = left;
+    }
+    if(right<n && arr[largest]<arr[right]){
+        largest = right;
+    }
+    
+    if(largest!=i){
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+
+void minHeapify(int arr[], int n, int i){
+    int smallest = i;
+    int left = 2*i;
+    int right = 2*i + 1;
+    
+    if(left<=n && arr[smallest]>arr[left]){
+        smallest = left;
+    }
+    if(right<=n && arr[smallest]>arr[right]){
+        smallest = right;
+    }
+    
+    if(smallest!=i){
+        swap(arr[smallest], arr[i]);
+        minHeapify(arr, n, smallest);
+    }
+}
 
 int main() {
 	heap h;
@@ -48,5 +110,26 @@ int main() {
 	h.insert(81);
 	
 	h.print();
+	
+	h.deleteFromHeap();
+	h.print();
+	
+	int arr[6] = {-1, 54, 53, 55, 52, 50};
+	int n=5;
+	for(int i = n/2;i>0;i--){
+	    heapify(arr, n, i);
+	}
+	for(int i=1;i<=n;i++){
+	    cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+	
+	for(int i = n/2;i>0;--i){
+	    minHeapify(arr, n, i);
+	}
+	for(int i=1;i<=n;i++){
+	    cout<<arr[i]<<" ";
+	}
+	cout<<endl;
 
 }
