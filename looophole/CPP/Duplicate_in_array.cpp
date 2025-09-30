40d87508e59b04db93485636d577e6f40be37ab3
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-09-30 02:28:27 -0500

    Update Duplicate_in_array.cpp

--- looophole/CPP/Duplicate_in_array.cpp
+++ looophole/CPP/Duplicate_in_array.cpp
@@ -1,5 +1,4 @@
-#include <set>
-
+// Solution 1:
 int findDuplicate(vector<int> &arr) 
 {
     int ans = 0;
@@ -10,4 +9,21 @@ int findDuplicate(vector<int> &arr)
         ans = ans ^ i;
     }
     return ans;
+}
+
+
+// Solution 2:
+
+#include <unordered_map>
+
+int findDuplicate(vector<int> &arr) 
+{
+    unordered_map<int,int> seen;
+    for(int x: arr){
+        seen[x]++;
+        if(seen[x]>1){
+            return x;
+        }
+    }
+    return -1;
 }
