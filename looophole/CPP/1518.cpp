d871bb26f05e394e80959ef216832325d1d54f42
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-10-01 11:50:53 -0500

    Create 1518.cpp

---
+++ looophole/CPP/1518.cpp
@@ -0,0 +1,15 @@
+// Water Bottles
+// Time: O(number of exchanges) ≤ O(numBottles) • Space: O(1).
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles;
+        while(numBottles>=numExchange){
+            int temp = numBottles/numExchange;
+            numBottles = numBottles%numExchange + temp;
+            ans += temp;
+        }
+        return ans;
+    }
+};
