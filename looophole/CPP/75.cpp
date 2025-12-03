76b5432b68e8d5a75dd19c99b7f0dc86c014b9e9
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-12-03 08:28:18 -0600

    Refactor sorting algorithms in 75.cpp

--- looophole/CPP/75.cpp
+++ looophole/CPP/75.cpp
@@ -1,93 +1,146 @@
-// Solution 1 -> Bubble Sort
+// Selection Sort
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         for(int i=0;i<nums.size()-1;i++){
+//             int index = i;
+//             for(int j=i+1;j<nums.size();j++){
+//                 if(nums[j]<nums[index]){
+//                     index = j;
+//                 }
+//             }
+//             swap(nums[i], nums[index]);
+//         }
+//     }
+// };
 
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        for(int i=1;i<nums.size();i++){
-            for(int j=0;j<nums.size()-i;j++){
-                if(nums[j]>nums[j+1]){
-                    swap(nums[j],nums[j+1]);
-                }
-            }
-        }
-    }
-};
-
-// Bubble sort with Recursion
-
-class Solution {
-public:
-
-    void sort(vector<int>& nums, int n){
-        // base case
-        if(n==0 || n==1){
-            return;
-        }
-        for(int i=0; i<n-1;i++){
-            if(nums[i]>nums[i+1]){
-                swap(nums[i], nums[i+1]);
-            }
-        }
-        sort(nums, n-1);
-    }
-
-    void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums, n);
-    }
-};
+//Decreasing selection sort
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         for(int i=0;i<nums.size()-1;i++){
+//             int index = i;
+//             for(int j=i+1;j<nums.size();j++){
+//                 if(nums[j]>nums[index]){
+//                     index = j;
+//                 }
+//             }
+//             swap(nums[i], nums[index]);
+//         }
+//     }
+// };
 
 
-// Solution 2 -> Insertion Sort
-/*
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            int temp = nums[i];
-            int j=i-1;
-            while(j>=0){
-                if(nums[j]>temp){
-                    nums[j+1] = nums[j];
-                }
-                else{
-                    break;
-                }
-                j-=1;
-            }
-            nums[j+1] = temp;
-        }
-    }
-};
+// // Bubble Sort
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         for(int i = nums.size() - 2;i >= 0; i--){
+//             bool swapped = 0;
+//             for(int j = 0; j<=i; j++){
+//                 if(nums[j]>nums[j+1]){
+//                     swap(nums[j], nums[j+1]);
+//                     swapped = 1;
+//                 }
+//             }
+//             if(swapped==0){
+//                 break;
+//             }
+//         }
+//     }
+// };
+
+// Decreasing Bubble Sort
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         for(int i = nums.size() - 2;i >= 0; i--){
+//             bool swapped = 0;
+//             for(int j = 0; j<=i; j++){
+//                 if(nums[j]<nums[j+1]){
+//                     swap(nums[j], nums[j+1]);
+//                     swapped = 1;
+//                 }
+//             }
+//             if(swapped==0){
+//                 break;
+//             }
+//         }
+//     }
+// };
+
+// Reverse Bubble Sort
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int i = 0;i <= n - 2; i++){
+//             bool swapped = 0;
+//             for(int j = 0; j < n-1-i; j++){
+//                 if(nums[j]>nums[j+1]){
+//                     swap(nums[j], nums[j+1]);
+//                     swapped = 1;
+//                 }
+//             }
+//             if(swapped==0){
+//                 break;
+//             }
+//         }
+//     }
+// };
+
+// Insertion Sort
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int i = 1; i<n; i++){
+//             for(int j = i; j>0; j--){
+//                 if(nums[j]<nums[j-1]){
+//                     swap(nums[j], nums[j-1]);
+//                 }
+//                 else{
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+// };
 
 
+// Decreasing order Insertion sort
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int i = 1; i<n; i++){
+//             for(int j = i; j>0; j--){
+//                 if(nums[j]>nums[j-1]){
+//                     swap(nums[j], nums[j-1]);
+//                 }
+//                 else{
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+// };
 
-// Solution 2 -> Insertion Sort (Using Recursion)
 
-#include <bits/stdc++.h>
-
-void sort(vector<int>& arr, int n, int s){
-    // base case
-    if(s>=n){
-        return;
-    }
-    int temp = arr[s];
-    int i = s-1;
-    while(i>=0){
-        if(arr[i]>temp){
-            arr[i+1] = arr[i];
-        }
-        else{
-            break;
-        }
-        i-=1;
-    }
-    arr[i+1] = temp;
-    sort(arr, n, s+1);
-}
-
-void insertionSort(int n, vector<int> &arr){
-    sort(arr, n, 1);
-}
-*/
+// Reverse Insertion sort
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int i=n-2;i>=0;i--){
+//             for(int j=i;j<n-1;j++){
+//                 if(nums[j] > nums[j+1]){
+//                     swap(nums[j], nums[j+1]);
+//                 }
+//                 else{
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+// };
