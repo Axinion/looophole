6bcfeb28a1bca92b22581a018b0cd186dd4a7320
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-12-09 04:39:06 -0600

    Add binary search solution for rotated array
    Implement binary search for target in rotated sorted array.

---
+++ looophole/CPP/33.cpp
@@ -0,0 +1,35 @@
+// 33. Search in Rotated Sorted Array
+
+// Binary Search Solution
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int ans = -1;
+        int s = 0;
+        int e = nums.size() - 1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid]>=nums[s]){
+                if(nums[s]<=target && target<=nums[mid]){
+                    e = mid - 1;
+                }
+                else{
+                    s = mid + 1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[e]){
+                    s = mid + 1;
+                }
+                else{
+                    e = mid - 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
