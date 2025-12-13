b84260290e61873e68cfd0d65d149cc0e2a00c01
Mihir Pandya <55135463+Axinion@users.noreply.github.com>
2025-12-13 02:21:44 -0600

    Implement minTime function for Painter's Partition Problem

--- looophole/CPP/AllocateMinimumPages.cpp
+++ looophole/CPP/AllocateMinimumPages.cpp
@@ -32,3 +32,40 @@ class Solution {
         return (int)ans;
     }
 };
+
+
+// GFG -> The Painter's Partition Problem-II
+
+
+class Solution {
+  public:
+    int minTime(vector<int>& arr, int k) {
+        int n = arr.size();
+        long long s = *max_element(arr.begin(), arr.end());
+        long long e = accumulate(arr.begin(), arr.end(), 0);
+        long long ans =-1;
+        if(n<k){
+            return -1;
+        }
+        while(s<=e){
+            long long mid = s + (e-s)/2;
+            long long board = 0;
+            int count = 1;
+            for(int x: arr){
+                board += x;
+                if(board>mid){
+                    count++;
+                    board = x;
+                }
+            }
+            if(count<=k){
+                ans = mid;
+                e = mid - 1;
+            }
+            else{
+                s = mid + 1;
+            }
+        }
+        return (int)ans;
+    }
+};
